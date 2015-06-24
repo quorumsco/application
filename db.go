8d46f99007919df4d47ff443f46f54cb27da38ed
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-06-24 13:17:45 +0200

    Fix middlewares and remove unused DB interface

--- db.go
+++
@@ -1,5 +0,0 @@
-package application
-
-// DB is the interface drivers must implement to be used with the
-// Application.
-type DB interface{}
--- middlewares.go
+++ middlewares.go
@@ -3,15 +3,22 @@ package application
 import (
 	"net/http"
 
+	"github.com/iogo-framework/application"
 	"github.com/iogo-framework/router"
 )
 
 // Apply is a middleware which make the app context available to the
-// requests handlers.
+// requests handlers through the router. Obviously, this stands for
+// iogo-framework/router.
 func (app *Application) Apply(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
-		router.GetContext(r).Env["Templates"] = app
+		router.GetContext(r).Env["Application"] = app
 		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
+}
+
+// App retrieves the Application stored by the middleware Apply.
+func App(r *http.Request) *Application {
+	return router.GetContext(r).Env["Application"].(*application.Application)
 }
