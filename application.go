e476bf4b7756a7a9cab358c04155e55faf0bf68f
Douézan-Grard Guillaume <silverwyrda@gmail.com>
2015-06-24 13:10:32 +0200

    Make application more customizable

--- application.go
+++ application.go
@@ -6,62 +6,58 @@ package application
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 
 	"github.com/iogo-framework/logs"
 )
 
-// Application an abstraction wrapper containing the router of your
-// choice, a database link and some templates.
+// Application is an abstraction layer containing the router of your
+// choice and additionnal components (templates, database links, etc).
 type Application struct {
-	Mux       Mux
-	DB        DB
-	Templates map[string]*template.Template
+	Mux        Mux
+	Components map[string]interface{}
 }
 
 // New creates a new Application with initialised fields.
 func New() (*Application, error) {
 	var app = new(Application)
-
-	app.Templates = make(map[string]*template.Template)
-
+	app.Components = make(map[string]interface{})
 	return app, nil
 }
 
 // Get defers to the router Mux the handling of requests matching the
 // pattern path, associated with the handle function.
-func (app *Application) Get(path string, handle http.HandlerFunc) {
+func (app *Application) Get(path interface{}, handle http.HandlerFunc) {
 	app.Mux.Get(path, handle)
 }
 
 // Post defers to the router Mux the handling of requests matching the
 // pattern path, associated with the handle function.
-func (app *Application) Post(path string, handle http.HandlerFunc) {
+func (app *Application) Post(path interface{}, handle http.HandlerFunc) {
 	app.Mux.Post(path, handle)
 }
 
 // Put defers to the router Mux the handling of requests matching the
 // pattern path, associated with the handle function.
-func (app *Application) Put(path string, handle http.HandlerFunc) {
+func (app *Application) Put(path interface{}, handle http.HandlerFunc) {
 	app.Mux.Put(path, handle)
 }
 
 // Patch defers to the router Mux the handling of requests matching the
 // pattern path, associated with the handle function.
-func (app *Application) Patch(path string, handle http.HandlerFunc) {
+func (app *Application) Patch(path interface{}, handle http.HandlerFunc) {
 	app.Mux.Patch(path, handle)
 }
 
 // Delete defers to the router Mux the handling of requests matching the
 // pattern path, associated with the handle function.
-func (app *Application) Delete(path string, handle http.HandlerFunc) {
+func (app *Application) Delete(path interface{}, handle http.HandlerFunc) {
 	app.Mux.Delete(path, handle)
 }
 
 // Options defers to the router Mux the handling of requests matching the
 // pattern path, associated with the handle function.
-func (app *Application) Options(path string, handle http.HandlerFunc) {
+func (app *Application) Options(path interface{}, handle http.HandlerFunc) {
 	app.Mux.Options(path, handle)
 }
 
@@ -77,7 +73,7 @@ func (app *Application) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 }
 
 // Serve start a listening server on port.
-func (app *Application) Serve(port int) {
-	logs.Info("Listening on http://localhost:%d", port)
+func (app *Application) Serve(host string, port int) {
+	logs.Info("Listening on http://%s:%d", host, port)
 	logs.Critical(http.ListenAndServe(fmt.Sprintf(":%d", port), app.Mux))
 }
--- mux.go
+++ mux.go
@@ -5,12 +5,12 @@ import "net/http"
 // Mux is the interface requests multiplexers must implement to be used
 // with Application.
 type Mux interface {
-	Get(path string, handle http.HandlerFunc)
-	Post(path string, handle http.HandlerFunc)
-	Put(path string, handle http.HandlerFunc)
-	Patch(path string, handle http.HandlerFunc)
-	Delete(path string, handle http.HandlerFunc)
-	Options(path string, handle http.HandlerFunc)
+	Get(path interface{}, handle http.HandlerFunc)
+	Post(path interface{}, handle http.HandlerFunc)
+	Put(path interface{}, handle http.HandlerFunc)
+	Patch(path interface{}, handle http.HandlerFunc)
+	Delete(path interface{}, handle http.HandlerFunc)
+	Options(path interface{}, handle http.HandlerFunc)
 	Use(handler func(http.Handler) http.Handler)
 	ServeHTTP(w http.ResponseWriter, req *http.Request)
 }
