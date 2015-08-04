c03629cc2d26c78fd4367a3b06ed48d1a42c5d7f
Poncholay <guillaume.wilmot@epitech.eu>
2015-08-04 13:54:28 +0200

    Wrap almost completed

--- middlewares.go
+++ middlewares.go
@@ -2,10 +2,7 @@ package application
 
 import (
 	"net/http"
-	"strconv"
 
-	"github.com/quorumsco/jsonapi"
-	"github.com/quorumsco/logs"
 	"github.com/quorumsco/router"
 )
 
@@ -15,37 +12,6 @@ import (
 func (app *Application) Apply(h http.Handler) http.Handler {
 	fn := func(w http.ResponseWriter, r *http.Request) {
 		router.Context(r).Env["Application"] = app
-		h.ServeHTTP(w, r)
-	}
-	return http.HandlerFunc(fn)
-}
-
-func (app Application) Cors(h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "access-control-allow-origin,content-type")
-		h.ServeHTTP(w, r)
-	}
-	return http.HandlerFunc(fn)
-}
-
-func (app *Application) SetUID(h http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, r *http.Request) {
-		var (
-			res    int
-			userID uint
-			err    error
-
-			query = r.URL.Query()
-		)
-		res, err = strconv.Atoi(query.Get("user_id"))
-		if err != nil {
-			logs.Debug(err)
-			jsonapi.Error(w, r, err.Error(), http.StatusBadRequest)
-			return
-		}
-		userID = uint(res)
-		router.Context(r).Env["UserID"] = userID
 		h.ServeHTTP(w, r)
 	}
 	return http.HandlerFunc(fn)
